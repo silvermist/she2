c7abe4673aee9084bfcfdad416356645b6fa8e85
mahmoud <mahmoud@dfki.uni-kl.de>
2015-11-02 19:06:36 +0200

    el bdaya

---
+++ AIProject1/src/Tile.java
@@ -0,0 +1,10 @@
+
+public class Tile {
+boolean movable;
+Type type;
+public Tile(boolean movable, Type type){
+	this.movable = movable;
+	this.type = type;
+}
+
+}
---
+++ AIProject1/src/Type.java
@@ -0,0 +1,4 @@
+
+public enum Type {
+	PATH, BLOCK, BLANK
+}
